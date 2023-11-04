440e7fd2b078f398f385214fa5131ea183544c13
Pascal JK <pascual.santos80@gmail.com>
2023-11-05 01:02:54 +0200

    - made improvements to main file.

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,28 @@
+import 'package:dynamic_theme_project/controllers/main_controller.dart';
+import 'package:dynamic_theme_project/pages/home.dart';
+import 'package:dynamic_theme_project/utils/themes/dark_theme.dart';
+import 'package:dynamic_theme_project/utils/themes/light_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 
 Future<void> main() async {
   await GetStorage.init();
-  runApp(const MainApp());
+  runApp(MainApp(controller: Get.put(MainController())));
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  const MainApp({super.key, required this.controller});
+  final MainController controller;
 
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: controller.getThemeData,
+      themeMode: controller.getThemeMode,
+      home: HomePage(
+        mainController: controller,
       ),
     );
   }
